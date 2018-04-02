694d5713ce37049dfc56c39455165f48e6e6e6a4
David Tumpowsky and Randy Weyer <randy@>
2018-04-02 15:24:02 -0700

    add address information

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,15 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
+function Address(street, city, state, zip) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
+
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -15,8 +23,13 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var home = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -25,9 +38,18 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".street").text(home.street);
+      $(".city").text(home.city);
+      $(".state").text(home.state);
+      $(".zip").text(home.zip);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
+
   });
 });
