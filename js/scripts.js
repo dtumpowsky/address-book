bd00cfd07d26cf868e8197622bba5c05dcb82f92
David Tumpowsky and Jimmy McNamara <jimmy@>
2018-04-03 10:08:53 -0700

    add feature to put an additional address with a contact entry

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,34 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + ", " + this.zip;
+}
 // user interface logic
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-street">Street</label>' +
+                                    '<input type="text" class="form-control" id="new-street">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control" id="new-city">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-state">State</label>' +
+                                    '<input type="text" class="form-control" id="new-state">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-zip">Zip Code</label>' +
+                                    '<input type="text" class="form-control" id="new-zip">' +
+                                  '</div>' +
+                                '</div>')
+
+
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -31,7 +57,18 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
     var home = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input#new-street").val();
+      var inputtedCity = $(this).find("input#new-city").val();
+      var inputtedState = $(this).find("input#new-state").val();
+      var inputtedZip = $(this).find("input#new-zip").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedCity);
+      newContact.addresses.push(newAddress);
+    });
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
@@ -42,6 +79,11 @@ $(document).ready(function() {
       $(".city").text(home.city);
       $(".state").text(home.state);
       $(".zip").text(home.zip);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        console.log("AN EXTRA ADDRESS");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("input#new-first-name").val("");
